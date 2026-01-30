81692f9e0c60b235885885e4b53f6e0e936710b2
Morphes <92577219+Morphess@users.noreply.github.com>
2026-01-30 16:10:42 +0200

    add Fahrenheit to Celsium conversion

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,8 +2,21 @@ package app;
 
 public class Main {
 
+    private static final double FREEZING_F = 32.0;
+
     public static void main(String[] args) {
         System.out.println("Converter v 1.0");
         System.out.println("App for temperature converting");
+
+        double f = 77.0;
+        double c = convFahrenheitToCelsium(f);
+
+        System.out.println("F > C:");
+        System.out.println(f + "F = " + c + "C");
+
+    }
+
+    private static double convFahrenheitToCelsium(double f) {
+        return (f - FREEZING_F) * 5.0/9.0;
     }
 }
